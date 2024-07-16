edabd5da55ff360343df26b6e1d60c3e7271daf0
SemkiShow <mgdeveloper123@gmail.com>
2024-07-16 13:04:51 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Init
+    const char* map[] = {
+        "##########",
+        "#   #     ",
+        "# ### ####",
+        "#        #",
+        "###### # #",
+        "#    # # #",
+        "# ## # # #",
+        "# #  # # #",
+        "  #    # #",
+        "##########"
+    };
+
+    std::cout << "Hello World!";
+    return 0;
+}
