3713ebd155ac829a20a2357caf0e86575dee84dc
SemkiShow <mgdeveloper123@gmail.com>
2024-07-16 17:03:47 +0300

    Added a ray collision detection function

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ string map[] = {
     "P #    # #",
     "##########"
 };
-int* playerCoords;
+int* playerPosition;
 int displayResolution[2] = {32, 18};
 int FOV = 120;
 int renderDistance = 5;
@@ -23,19 +23,43 @@ int renderDistance = 5;
 int* AngleToRayDestination(int angle)
 {
     static int _rayDestination[2];
-    _rayDestination[0] = playerCoords[0] + cos(angle) * renderDistance;
-    _rayDestination[1] = playerCoords[1] + sin(angle) * renderDistance;
+    _rayDestination[0] = playerPosition[0] + cos(angle) * renderDistance;
+    _rayDestination[1] = playerPosition[1] + sin(angle) * renderDistance;
     return _rayDestination;
 }
 
-int RayCollisionDetection(/* int destinationX, int destinationY */)
+int* RayCollisionDetection(int rayDestination[])
 {
-
+    float _rayStep[2];
+    _rayStep[0] = (rayDestination[0] - playerPosition[0]) * 1.0 / renderDistance;
+    _rayStep[1] = (rayDestination[1] - playerPosition[1]) * 1.0 / renderDistance;
+    
+    static int _rayPosition[2];
+    _rayPosition[0] = -1;
+    _rayPosition[1] = -1;
+    for (int i = 0; i < renderDistance; i++)
+    {
+        _rayPosition[0] = playerPosition[0] + (int)(_rayStep[0] * i);
+        _rayPosition[1] = playerPosition[1] + (int)(_rayStep[1] * i);
+        if (_rayPosition[0] < 0 | _rayPosition[0] > map[0].length() | _rayPosition[1] < 0 | _rayPosition[1] > (sizeof(map) / sizeof(*map)))
+        {
+            break;
+        }
+        else
+        {
+            if (map[_rayPosition[1]][_rayPosition[0]] == '#')
+            {
+                break;
+            }
+        }
+    }
+    return _rayPosition;
+    
 }
 
 int* GetPlayerPosition()
 {
-    static int _playerCoords[2] = {-1, -2};
+    static int _playerPosition[2] = {-1, -2};
     
     for (int y = 0; y < (sizeof(map) / sizeof(*map)); y++)
     {
@@ -44,33 +68,44 @@ int* GetPlayerPosition()
             // cout << map[y][x];
             if (map[y][x] == 'P')
             {
-                _playerCoords[0] = x;
-                _playerCoords[1] = y;
+                _playerPosition[0] = x;
+                _playerPosition[1] = y;
                 break;
             }
             
         }
-        if (_playerCoords[0] != -1)
+        if (_playerPosition[0] != -1)
         {
             break;
         }
         
         // cout << endl;
     }
-    return _playerCoords;
+    return _playerPosition;
 }
 
 int main()
 {
     // Init
-    playerCoords = GetPlayerPosition();
-    cout << "Player position: " << playerCoords[0] << ", " << playerCoords[1] << endl;
+    playerPosition = GetPlayerPosition();
+    cout << "Player position: " << playerPosition[0] << ", " << playerPosition[1] << endl;
     
     int _angle;
-    cout << "Enter angle: ";
-    cin >> _angle;
+    bool _askForAngle = false;
+    if (_askForAngle)
+    {
+        cout << "Enter angle: ";
+        cin >> _angle;
+    }
+    else
+    {
+        _angle = 42; // Veritasium angle
+    }
     int* _rayDestination = AngleToRayDestination(_angle);
-    cout << "Ray destination of angle " << _angle << " is " << _rayDestination[0] << ", " << _rayDestination[1] << endl;
+    cout << "Ray destination at angle " << _angle << " is " << _rayDestination[0] << ", " << _rayDestination[1] << endl;
+
+    int* _rayPosition = RayCollisionDetection(_rayDestination);
+    cout << "Ray position is " << _rayPosition[0] << ", " << _rayPosition[1];
 
     return 0;
 }
