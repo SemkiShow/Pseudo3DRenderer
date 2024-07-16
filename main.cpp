3997420f3d27de01386af23df31458682ddcdc7e
SemkiShow <mgdeveloper123@gmail.com>
2024-07-16 13:57:37 +0300

    Added a player position fetching function

--- main.cpp
+++ main.cpp
@@ -1,22 +1,64 @@
 #include <iostream>
 using namespace std;
 
+// Public variables init
+string map[] = {
+    "##########",
+    "#   #     ",
+    "# ### ####",
+    "#        #",
+    "###### # #",
+    "#    # # #",
+    "# ## # # #",
+    "# #  # # #",
+    "P #    # #",
+    "##########"
+};
+int* playerCoords;
+
+int* DegreesToDestination(/* int playerX, int playerY, int degree, int renderDistance */)
+{
+
+}
+
+int RayCollisionDetection(/* int destinationX, int destinationY */)
+{
+
+}
+
+int* GetPlayerPosition()
+{
+    static int _playerCoords[2] = {-1, -2};
+    
+    for (int y = 0; y < (sizeof(map) / sizeof(*map)); y++)
+    {
+        for (int x = 0; x < map[y].length(); x++)
+        {
+            // cout << map[y][x];
+            if (map[y][x] == 'P')
+            {
+                _playerCoords[0] = x;
+                _playerCoords[1] = y;
+                break;
+            }
+            
+        }
+        if (_playerCoords[0] != -1)
+        {
+            break;
+        }
+        
+        // cout << endl;
+    }
+    return _playerCoords;
+}
+
 int main()
 {
     // Init
-    const char* map[] = {
-        "##########",
-        "#   #     ",
-        "# ### ####",
-        "#        #",
-        "###### # #",
-        "#    # # #",
-        "# ## # # #",
-        "# #  # # #",
-        "  #    # #",
-        "##########"
-    };
+    playerCoords = GetPlayerPosition();
+    cout << "Player position: " << playerCoords[0] << ", " << playerCoords[1] << endl;
 
-    std::cout << "Hello World!";
+
     return 0;
 }
